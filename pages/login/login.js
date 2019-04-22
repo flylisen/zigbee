954c18cc5a2c011947faef2de40fc6739f5d58af
zoujiacai <807102365@qq.com>
2019-04-22 15:52:26 +0800

    补提交

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,44 +1,49 @@
 const app = getApp();
-
 Page({
-
   data: {
      username:null,
-     password:null
+     password:null,
+     id_token:'',
+     response:''
   },
   usernameInput:function(event){
-     this.setData({username:event.detail.value})
+     this.setData({
+       username:event.detail.value
+       })
   },
   passwordInput: function (event) {
-    this.setData({password: event.detail.value})
+    this.setData({
+      password: event.detail.value
+      })
   },
   //登录
   loginBtnClick:function(){
     var that=this
-    if (this.data.username == null || this.data.password==null){
-       wx.showToast({
-         title: '用户名或者密码不能为空',
-         icon:'loading',
-         duration:2000
-       })
-    }else{
       wx.request({
         url: 'https://dev.rishuncloud.com:8443/login',
         data:{
           username:this.data.username,
           password:this.data.password,
         },
-        method:'GET'
-      })
-      wx.showToast({
-        title: '登录成功',
-        icon:'success',
-        duration:2000
-      })
-      app.appData.userInfo = { username: this.data.username }
-      wx.switchTab({
-        url: '../index/index'
-      })
-    }
+        method:'POST',
+        success:function(res){
+            this.setData({
+                id_token:res.data.id_token,
+                response:res                 
+            })
+            try{
+              wx.setStorageSync('id_token', res.data.id_token)
+            } catch (e){    
+            }    
+          wx.switchTab({
+            url: '../index/index'
+          })
+          console.log(res.data);
+          },
+          fail:function(res){
+            console.log(res.data);
+            console.log('is failed')
+          }
+        }) 
   }
 })
