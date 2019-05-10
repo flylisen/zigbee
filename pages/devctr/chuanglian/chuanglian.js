ea5209091b7c3b25e234861fd3a270ae902a8942
zoujiacai <807102365@qq.com>
2019-05-10 14:40:16 +0800

    更新

---
+++ pages/devctr/chuanglian/chuanglian.js
@@ -0,0 +1,104 @@
+var Industrys;
+const app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    diNames:'',
+    chuanglians:''
+  },
+  
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var deviceuid = decodeURIComponent(options.deviceuid);
+    Industrys = JSON.parse(deviceuid);
+    this.setData({
+      diNames: Industrys.diName,
+      chuanglians: Industrys.diOnoffStatu
+    })
+  },
+  chuangliang: function (a) {
+    var that = this
+    var username = wx.getStorageSync('username');//网关账号
+    var pwd = wx.getStorageSync('pwd'); //网关密码
+    if (Industrys.diOnoffStatu >= 1) {
+      Industrys.diOnoffStatu = 0
+    } else {
+      Industrys.diOnoffStatu = 1
+    }
+    console.log(Industrys.diOnoffStatu)
+    this.setData({
+      chuanglians:Industrys.diOnoffStatu
+    })
+    let url = app.globalData.URL + 'ctrDev';
+    let data = {
+      bindid: username,
+      bindstr: pwd,
+      ctrType: 0,
+      devs: [{ deviceuid: Industrys.diDeviceuid, value: Industrys.diOnoffStatu }]
+    };
+    app.wxRequest('POST', url, data, (res) => {
+      console.log(res.data)
+    },
+      (err) => {
+        console.log(err.errMsg)
+      }
+    )
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    //回调
+    app.globalData.onReceiveWebsocketMessageCallback = function (res) {
+      console.log('接收到服务器信息', res);
+      console.log('当前页面在chouanglian');
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
